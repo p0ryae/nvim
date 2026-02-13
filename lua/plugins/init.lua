97e3bd1b750d81336d6ef777d03f0d4aa06eb3ea
p0ryae <contact@porya.me>
2026-02-13 11:44:20 -0800

    remove latex plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -42,14 +42,6 @@ return {
   },
 
   {
-    "lervag/vimtex",
-    lazy = false,
-    init = function()
-      vim.g.vimtex_view_method = "zathura"
-    end
-  },
-
-  {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
     opts = {
